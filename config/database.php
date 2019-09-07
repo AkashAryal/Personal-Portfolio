11dec797ef96d3b105c14621a3325220d1741e15
Akash Aryal <aryal.akash4414@gmail.com>
2019-09-07 19:57:33 -0400

    heroku buildpack

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
     | Here you may specify which of the database connections below you wish
     | to use as your default connection for all database work. Of course
     | you may use many connections at once using the Database library.
-    |
+    |//
     */
 
     //'default' => env('DB_CONNECTION', 'pg'), //for localVersion
